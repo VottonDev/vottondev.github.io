3acf6d12f8c428ccf3e1e1412d2d3633a95e0973
Otto Hooper <votton.hooper@gmail.com>
2023-06-08 13:49:01 +0100

    Remove extra character from href

--- pages/index.js
+++ pages/index.js
@@ -153,7 +153,7 @@ export default function Home() {
           keybase &mdash; <a href="https://keybase.io/Votton">Votton</a>
         </li>
         <li>
-          github &mdash; <a href="https://github.com/VottonDev>">VottonDev</a>
+          github &mdash; <a href="https://github.com/VottonDev">VottonDev</a>
         </li>
         <li>
           patreon &mdash; <a href="https://patreon.com/Votton">Votton</a>
